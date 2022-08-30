605e353d43c7c7c43a8e0e6a49c9f75639e380a2
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-08-30 13:56:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Socket Queue
+- App to take turns in business.
+
+### Elis Antonio Perez
+
+#### Follow me [Instagram](http://instagram.com/elisperezmusic)
---
+++ models/ticket-control.js
@@ -0,0 +1,28 @@
+class TicketControl {
+  constructor() {
+    this.ultimo = 0;
+    this.hoy = new Date().getDate();
+    this.tickets = [];
+    this.ultimos4 = [];
+
+    this.init;
+  }
+
+  get toJson() {
+    return {
+      ultimo: this.ultimo,
+      hoy: this.hoy,
+      tickets: this.tickets,
+      ultimos4: this.ultimos4,
+    };
+  }
+
+  init() {
+    const data = require('../db/data.json');
+    console.log(data);
+  }
+
+  guardarDB() {
+
+  }
+}
